52854264ae82c67f4ab9e4bb1e75f8d3cd30c245
@siddharth.hota <siddharth.hota@nettantra.net>
2023-06-02 12:06:48 +0530

    Update github link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className='text-center'>
         <h1 className='font-bold text-3xl my-10'>Wellcome to the Redux toolkit Store</h1>
-        <h2 className='text-gray-800 font-mono'><strong>source code is available on <a href="https://github.com/Sidhota54/React_Redux"><span className='text-blue-700 underline hover:text-gray-500'>github</span></a></strong></h2>
+        <h2 className='text-gray-800 font-mono'><strong>source code is available on <a href="https://github1s.com/Sidhota54/Redux_Ecommerce/blob/HEAD/src/App.js"><span className='text-blue-700 underline hover:text-gray-500'>github</span></a></strong></h2>
         <section>
             <h3 className='font-semibold text-2xl mb-5'>Products</h3>
             <Products />
@@ -13,5 +13,4 @@ const Home = () => {
     </div>
   )
 }
-
 export default Home
