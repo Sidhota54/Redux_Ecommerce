99772dd7cf38d166ceac22a1643856abae9ae80c
@siddharth.hota <siddharth.hota@nettantra.net>
2023-06-02 11:54:47 +0530

    add git source code Link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,8 @@ import Products from '../Components/Products'
 const Home = () => {
   return (
     <div className='text-center'>
-        <h2 className='font-bold text-3xl my-10'>Wellcome to the Redux toolkit Store</h2>
+        <h1 className='font-bold text-3xl my-10'>Wellcome to the Redux toolkit Store</h1>
+        <h2 className='text-gray-800 font-mono'><strong>source code is available on <a href="https://github.com/Sidhota54/React_Redux"><span className='text-blue-700 underline hover:text-gray-500'>github</span></a></strong></h2>
         <section>
             <h3 className='font-semibold text-2xl mb-5'>Products</h3>
             <Products />
