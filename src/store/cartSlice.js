9e05b4a72bd1b09b5f9074b489bf0aba172a4f94
@siddharth.hota <siddharth.hota@nettantra.net>
2023-06-08 16:11:15 +0530

    Add Cart details store in Localstorage

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,20 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
+const getCartItems = () => {
+    let localCartData = localStorage.getItem('items');
+    const parsedData = JSON.parse(localCartData);
+    if (!Array.isArray(parsedData)) return [];
+    return parsedData;
+}
+
+
 const cartSlice = createSlice({
     name: "cart",
-    initialState: [],
-    reducers:{
-        add(state, action){
+    initialState: getCartItems(),
+    reducers: {
+        add(state, action) {
             state.push(action.payload);
+            localStorage.setItem('items', JSON.stringify(state));
         },
-        remove(state, action){
-            return state.filter(item => item.id !== action.payload);
+        remove(state, action) {
+            
+            const updateState =  state.filter(item => item.id !== action.payload);
+            localStorage.setItem('items', JSON.stringify(updateState));
+            return updateState;
         },
     }
 })
 
-export const {add,remove} = cartSlice.actions;
+export const { add, remove } = cartSlice.actions;
 
 export default cartSlice.reducer;
 
